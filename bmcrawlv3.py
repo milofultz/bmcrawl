86f8a7a207cb6ef6a9cc309bc67f76570e5fb833
Milo Fultz <milopfultz@gmail.com>
2020-07-03 01:21:38 -0700

    added a filter for duplicate links

--- bmcrawlv3.py
+++ bmcrawlv3.py
@@ -2,7 +2,9 @@ from bs4 import BeautifulSoup
 import random
 import requests
 from requests.exceptions import ConnectionError
+from urllib.parse import urljoin
 import webbrowser
+
 
 def bm_input_handler(filename):
     with open(filename, 'r') as f:
@@ -29,9 +31,9 @@ def url_input_handler():
     #send back usable data
     return urls
 
-bad_links = ['facebook.com', 'twitter.com', 'instagram.com', 'mailto:', 'wiki']
+bad_links = ['facebook.com', 'twitter.com', 'instagram.com', 'youtube.com', 'mailto:', 'wiki']
 
-def scrape(html, from_title):
+def scrape(html, from_title, from_url=None):
 
     url_list = []
 
@@ -39,12 +41,12 @@ def scrape(html, from_title):
 
     for i, link in enumerate(soup.find_all('a')):
         
-        link_url = link.get('href')
+        link_url = absolute_url(link.get('href'), from_url)
         
         if i >= 50:
             print(link_url + ' has too many links.')
             break
-        
+
         if link_url == None \
         or any(substring in link_url for substring in bad_links) \
         or '//' not in link_url:
@@ -52,6 +54,14 @@ def scrape(html, from_title):
         url_list.append((link.text, link_url, from_title))
 
     return url_list
+
+def absolute_url(url, from_url):
+    if from_url == None:
+        return url
+    
+    filtered_url = urljoin(from_url, url)
+
+    return filtered_url
 
 
 def crawler(start_urls, depth=1):
@@ -69,12 +79,12 @@ def crawler(start_urls, depth=1):
 
 def crawl(start_urls):
     found_urls = []
-    
+
     for title, link, _ in start_urls:
         html = getpage(link)
         if html == None:
             continue
-        found_urls.extend(scrape(html, title))
+        found_urls.extend(scrape(html, title, link))
 
     return found_urls
 
@@ -99,7 +109,8 @@ footer = '</ul></body></html>'
 
 def output_handler(urls, number):
     random.shuffle(urls)
-    
+    urls = remove_duplicates(urls)
+
     with open(f'newlinks_{number}.html', 'w') as f:
         f.write(header)
         for title, url, from_title in urls:
@@ -109,6 +120,21 @@ def output_handler(urls, number):
         f.write(footer)
 
     chrome(f'newlinks_{number}.html')
+
+def remove_duplicates(lst): 
+    #Removes exact copies
+    lst = [t for t in (set(tuple(i) for i in lst))]
+    
+    existing_urls = set()
+    filtered_lst = []
+
+    for title, url, from_title in lst: 
+        if not url in existing_urls: 
+            existing_urls.add(url) 
+            filtered_lst.append((title, url, from_title)) 
+
+    return filtered_lst
+
 
 
 if __name__ == '__main__':
